2a3dd17d19c117cec586880224d31180d165f812
Gabriel Fernandez <gfernandez@impulso.edu.uy>
2024-08-27 11:05:10 +0000

    Tue, Aug 27, 2024, 11:05 AM +00:00

--- tambo4T/jajatonto.java
+++ tambo4T/jajatonto.java
@@ -7,13 +7,13 @@ public class jajatonto {
 	public static void main(String[] args) {
 		Scanner tec = new Scanner(System.in);
 		Scanner tec2 = new Scanner(System.in);
-		
+
 		//variables:
 		String tdd, tdc;
 		int opcion1, opcion2;
 		String departamento0, departamento1, departamento2, departamento3, departamento4, departamento5, departamento6, departamento7, departamento8, departamento9, departamento12, departamento13, departamento14, departamento15, departamento16, departamento17, departamento18, departamento19;
 		String ciudadn0, ciudadn1, ciudadn2, ciudadn3, ciudadn4, ciudadn5, ciudadn6, ciudadn7, ciudadn8, ciudadn9, ciudadn10, ciudadn11, ciudadn12, ciudadn13, ciudadn14, ciudadn15, ciudadn17, ciudadn18, ciudadn19;
-		
+
 		//Menu
 		System.out.println("Ingree una opcion:");
 		System.out.println("1. Registrar ciudad.");
@@ -22,24 +22,24 @@ public class jajatonto {
 		System.out.println("4. regresar");
 		System.out.println(" ");
 		opcion1 = tec.nextInt();
-		
+
 		//elimiar ciudad
 		switch (opcion1) {
 		case 1:
-			
+
 		case 2:
-			System.out.println("ingrese una ciudad:");
+			System.out.println("ingrese un departamento:");
 			tdc = tec2.nextLine();
-			
+
 		}
 
 	}
-	
+
 	public class ciudad{
 		Scanner tec1 = new Scanner(System.in);
 		Scanner tec2 = new Scanner(System.in);
-		
-		
+
+
 	}
 
 }
