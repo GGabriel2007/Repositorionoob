976c84a592d388f63fe0f452accce95684157997
Gabriel Fernandez <gfernandez@impulso.edu.uy>
2024-08-27 11:59:23 +0000

    Tue, Aug 27, 2024, 11:59 AM +00:00

--- tambo4T/jajatonto.java
+++ tambo4T/jajatonto.java
@@ -13,7 +13,7 @@ public class jajatonto {
         int opcion1, opcion2;
         String departamento0, departamento1, departamento2, departamento3, departamento4, departamento5, departamento6, departamento7, departamento8, departamento9, departamento12, departamento13, departamento14, departamento15, departamento16, departamento17, departamento18, departamento19;
         String ciudadn0 = "Vacio", ciudadn1= "Vacio",ciudadn2 = "Vacio", ciudadn3, ciudadn4, ciudadn5, ciudadn6, ciudadn7, ciudadn8, ciudadn9, ciudadn10, ciudadn11, ciudadn12, ciudadn13, ciudadn14, ciudadn15, ciudadn17, ciudadn18, ciudadn19;
-
+		int ciudadid0 = 0, ciudadid1 = 0, ciudadid2 = 0, ciudadid3 = 0,ciudadid4 = 0, ciudadid5 = 0, ciudadid6 = 0, ciudadid7 = 0, ciudadid8 = 0, ciudadid9 = 0, ciudadid10 = 0, ciudadid9 = 0, ciudadid11 = 0, ciudadid9 = 0, ciudadid12 = 0, ciudadid9 = 0, ciudadid13 = 0, ciudadid9 = 0, ciudadid14 = 0, ciudadid9 = 0, ciudadid15 = 0, ciudadid9 = 0, ciudadid16 = 0, ciudadid9 = 0, ciudadid17 = 0, ciudadid9 = 0, ciudadid18 = 0, ciudadid9 = 0, ciudadid19 = 0;
         //Menu
         System.out.println("Ingrese una opcion:");
         System.out.println("1. Registrar ciudad.");
@@ -55,7 +55,7 @@ public class jajatonto {
                     case 1:
                         System.out.println("¿Que ciudad desea eliminar?");
 						if (!ciudadn1.equals("vacio") && departamento1 == 1){
-							System.out.println("|(1) "+ciudadn1);
+							System.out.println("|("+ciudadid1+") "+ciudadn1);
 						}
                         break;
                     default:
