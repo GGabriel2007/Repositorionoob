7fc817165c738fc3920aa8836b57e538a61554cf
Gabriel Fernandez <gfernandez@impulso.edu.uy>
2024-10-10 17:58:54 +0000

    Thu, Oct 10, 2024, 5:58 PM +00:00

--- tambo4T/ciudadresult.java
+++ tambo4T/ciudadresult.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class ciudadresult {
     public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in)
+        Scanner tec1 = new Scanner(System.in);
+        Scanner tec2 = new Scanner(System.in);
     }
 }
