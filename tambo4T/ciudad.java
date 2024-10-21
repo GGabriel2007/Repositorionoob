3715e32b4bfb873e708e8b03a915a430f53a0278
Gabriel Fernandez <gfernandez@impulso.edu.uy>
2024-10-21 15:57:06 +0000

    Mon, Oct 21, 2024, 3:57 PM +00:00

---
+++ tambo4T/ciudad.java
@@ -0,0 +1,29 @@
+package test_zone;
+
+import java.util.*;
+
+public class gabriel_testzone {
+
+	public static void main(String[] args) {
+
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public class ciudad {
+		Scanner tec1 = new Scanner(System.in);
+		Scanner tec2 = new Scanner(System.in);
+
+		// COMFIGURACION DE VARIABLES
+		int casillas = 30;
+		int ciudades[] = new int[casillas];
+		int tambos[] = new int[casillas * 2];
+
+		int entrada1 = 0, opcion = 0; String entrada2 = "";
+
+		//MENU
+		System.out.println("_MENU_");
+		System.out.println("m");
+	}
+
+}
