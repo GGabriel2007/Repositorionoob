c16e93d6eed6ea96d43cd0be462bc1a3cea5108d
Gabriel Fernandez <gfernandez@impulso.edu.uy>
2024-10-25 18:54:24 +0000

    Fri, Oct 25, 2024, 6:54 PM +00:00

--- tambo4T/ciudad.java
+++ tambo4T/ciudad.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class ciudad {
 
 	public static void main(String[] args) {
-		
+
 		int casillas = 30;
-		
+
 		String nombre[] = new String[casillas];
 		String departamento[] = new String[casillas];
 		int centros[] = new int[casillas];
@@ -14,9 +14,6 @@ public class ciudad {
 		int posy[] = new int[casillas];
 		[] = new [casillas];
 		
-		
-		
-
 	}
 
 }
